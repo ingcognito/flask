cef0a8bb52573bb2e01d1358125e287c9a009956
noah-ing <ing.noah@gmail.com>
2020-09-02 17:50:42 -0400

    displays all fields in blog post

--- app/blog.py
+++ app/blog.py
@@ -11,7 +11,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, notes, created, author_id, username'
+        'SELECT p.id, title, substance, dosage, duration, locations, activities, realizations, notes, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
